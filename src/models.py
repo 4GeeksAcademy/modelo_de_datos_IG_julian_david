8f958728072f69870bdd5bcb15c6b8e0ac0aa276
Julián David Pineda Betancurth <89669806+juliandavidpineda@users.noreply.github.com>
2025-08-04 20:12:30 +0000

    new class :fire:

--- src/models.py
+++ src/models.py
@@ -6,10 +6,11 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    first_name: Mapped[str] = mapped_column(String(120))
-    last_name: Mapped[str] = mapped_column(String(120))
+    user_name: Mapped[str] = mapped_column(String(16) unique=True, nullable=False)
+    first_name: Mapped[str] = mapped_column(String(120) unique=True, nullable=False)
+    last_name: Mapped[str] = mapped_column(String(120)unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
+    post: Mapped["Post"] = db.relationship(back_populates="user")
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
 
@@ -17,6 +18,7 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "user_name": self.user_name,
             "first_name": self.first_name,
             "last_name": self.last_name
             # do not serialize the password, its a security breach
@@ -25,9 +27,20 @@ class User(db.Model):
 class Post(db.Model):
     id: Mapped[int] =mapped_column(primary_key=True)
 
+    user_id: Mapped[int] = mapped_column(db.ForeignKey("user.id"))
+    author: Mapped["User"] = db.relationship(back_populates="posts")
+ 
+
 
     def serialize(self):
         return{
             "id": self.id,
             
         }
+    
+class Follower(db.Model):
+    user_from_id: Mapped[int] = mapped_column(primary_key=True)
+
+
+
+    user_to_id: Mapped[int] = mapped_column(primary_key=True)
